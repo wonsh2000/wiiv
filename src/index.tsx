816e99f7a5041dc8536560d31bea7646d223ce52
wonsh2000 <wonsh2000@gmail.com>
2025-03-16 00:29:33 +0900

    중간 커밋

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './i18n';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Failed to find the root element');
+}
+
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
